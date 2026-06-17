5873154569c44f9a6a04a25123feaf5ac5c80c20
robeSystems <raunyhenriqueroberaio@gmail.com>
2026-06-17 19:51:04 -0300

    Setima versão

---
+++ replit.nix
@@ -0,0 +1,17 @@
+{pkgs}: {
+  deps = [
+    pkgs.mesa
+    pkgs.libxkbcommon
+    pkgs.libgbm
+    pkgs.atk
+    pkgs.at-spi2-atk
+    pkgs.alsa-lib
+    pkgs.cairo
+    pkgs.pango
+    pkgs.libdrm
+    pkgs.cups
+    pkgs.nspr
+    pkgs.nss
+    pkgs.glib
+  ];
+}
